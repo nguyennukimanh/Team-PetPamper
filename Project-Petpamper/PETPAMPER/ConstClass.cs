5428838fd21566611d9a9b9773f65164b8b8f950
nguyennukimanh <59416618+nguyennukimanh@users.noreply.github.com>
2020-05-05 11:40:19 +0700

    update

---
+++ Project-Petpamper/PETPAMPER/ConstClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetPamper
+{
+    public class ConstClass
+    {
+        public static string PETPAMPER_EMAIL_ADDRESS = "kimanh7012@gmail.com";
+        public static string PETPAMER_EMAIL_PASSWORD = "nguyennukimanh";
+    }
+}
---
+++ Project-Petpamper/PETPAMPER/Helper.cs
@@ -0,0 +1,101 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PetPamper
+{
+    public static class Helper
+    {
+
+        static readonly String _EncryptionKey = "vietnam@123";
+
+        public static string Encrypt(string clearText)
+        {
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(_EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+        public static string Decrypt(string cipherText)
+        {
+            cipherText = cipherText.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(_EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+
+        public static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+
+        public static void SendEmail(string emailUser, string newPassword)
+        {
+            string senderID = ConstClass.PETPAMPER_EMAIL_ADDRESS;
+            string senderPassword = ConstClass.PETPAMER_EMAIL_PASSWORD;
+
+            var content = "<p>We heard that you lost your password. Sorry about that!</p>"
+                            + "\n<p>Your new password is:</p>" 
+                            + $"\n<b>{newPassword}</b>" 
+                            + "\n<p>Please use it to login and change to new password.</p>"
+                            + "\n<p>Many Thanks</p>"
+                            + "\n<p>PetPamper Support Team</p>"; 
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(emailUser); 
+                mail.From = new MailAddress(senderID);
+                mail.Subject = "Your PetPamper's new password";
+                mail.Body = content;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com"; //Or Your SMTP Server Address
+                smtp.Credentials = new System.Net.NetworkCredential(senderID, senderPassword);
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                // Todo: 
+            }
+        }
+    }
+}
